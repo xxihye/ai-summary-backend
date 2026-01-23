83b9369e79e15dbd52f0fe5b6a4ef2ff69f00f5b
xxihye <ekfhdwld@gmail.com>
2026-01-23 14:36:38 +0900

    feat(api): add redis stream producer

---
+++ api/src/main/java/online/xxihye/infra/redis/JobQueueClient.java
@@ -0,0 +1,33 @@
+package online.xxihye.infra.redis;
+
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.RecordId;
+import org.springframework.data.redis.connection.stream.StreamRecords;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class JobQueueClient {
+
+    private static final String STREAM_KEY = "stream:sum:jobs";
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public JobQueueClient(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public RecordId enqueue(Long jobId) {
+        Map<String, String> fields = Map.of("jobId", String.valueOf(jobId));
+
+        ObjectRecord<String, Map<String, String>> record =
+            StreamRecords.newRecord()
+                         .ofObject(fields)
+                         .withStreamKey(STREAM_KEY);
+
+        return redisTemplate.opsForStream().add(record);
+    }
+}
---
+++ api/src/main/java/online/xxihye/infra/redis/RedisConfig.java
@@ -0,0 +1,26 @@
+package online.xxihye.infra.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(cf);
+
+        StringRedisSerializer s = new StringRedisSerializer();
+        template.setKeySerializer(s);
+        template.setValueSerializer(s);
+        template.setHashKeySerializer(s);
+        template.setHashValueSerializer(s);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
