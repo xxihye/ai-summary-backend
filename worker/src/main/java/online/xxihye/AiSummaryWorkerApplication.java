c436395c60d15e085385531f9b04bd3c3989d48d
xxihye <ekfhdwld@gmail.com>
2026-01-23 14:38:37 +0900

    chore(worker): add spring boot entrypoint and base config

---
+++ worker/src/main/java/online/xxihye/AiSummaryWorkerApplication.java
@@ -0,0 +1,12 @@
+package online.xxihye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSummaryWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSummaryWorkerApplication.class, args);
+    }
+}
