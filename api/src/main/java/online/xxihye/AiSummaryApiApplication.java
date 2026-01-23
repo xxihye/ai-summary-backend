813e43396c1ca9b611027c25d2cf697dca3b6724
xxihye <ekfhdwld@gmail.com>
2026-01-23 14:36:27 +0900

    chore(api): add spring boot entrypoint and base config

---
+++ api/src/main/java/online/xxihye/AiSummaryApiApplication.java
@@ -0,0 +1,15 @@
+package online.xxihye;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class AiSummaryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSummaryApiApplication.class, args);
+    }
+}
