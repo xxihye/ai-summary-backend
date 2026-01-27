47fc2a81bd8d380aab26860d52d346ed62e151a7
xxihye <ekfhdwld@gmail.com>
2026-01-27 17:01:26 +0900

    chore(worker): remove unused retry configuration

--- worker/src/main/java/online/xxihye/worker/config/RetryConfig.java
+++
@@ -1,9 +0,0 @@
-package online.xxihye.worker.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.EnableRetry;
-
-@EnableRetry
-@Configuration
-public class RetryConfig {
-}
